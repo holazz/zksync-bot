75ea0cfa5ebbd4d233fcd698430ad7ec1e44ae9d
holazz <2418184580@qq.com>
2023-10-16 22:11:07 +0800

    feat: add modules

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -4,6 +4,11 @@ import zksdomain from './zksdomain'
 import crossChainNFT from './crossChainNFT'
 import mintSquare from './mintSquare'
 import nfts2me from './nfts2me'
+import mailzero from './mailzero'
+import popsocial from './popsocial'
+import { tavaeraID, tavaeraNFT } from './tavaera'
+import omnisea from './omnisea'
+import l2telegraph from './l2telegraph'
 
 export default [
   dmail,
@@ -12,4 +17,10 @@ export default [
   crossChainNFT,
   mintSquare,
   nfts2me,
+  mailzero,
+  popsocial,
+  tavaeraNFT,
+  tavaeraID,
+  omnisea,
+  l2telegraph,
 ].sort((a, b) => a.value.localeCompare(b.value))
