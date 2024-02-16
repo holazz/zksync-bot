2aa76045e62ce8eb3458dd2d2d670ed65d42570c
holazz <2418184580@qq.com>
2024-02-16 11:50:53 +0800

    chore: update

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,16 +1,16 @@
-// import syncswap from './syncswap'
-// import zksdomain from './zksdomain'
-// import omnisea from './omnisea'
-// import safe from './safe'
+import syncswap from './syncswap'
+import zksdomain from './zksdomain'
+import omnisea from './omnisea'
+import safe from './safe'
 import erc20Approval from './erc20-approval'
 import erc721Approval from './erc721-approval'
 import dmail from './dmail'
 
 export default [
-  // syncswap,
-  // zksdomain,
-  // omnisea,
-  // safe,
+  syncswap,
+  zksdomain,
+  omnisea,
+  safe,
   ...erc20Approval,
   ...erc721Approval,
   dmail,
