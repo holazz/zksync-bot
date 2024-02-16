1d9d2498ca94c1cbdfd7605d8fb6c8471244badb
holazz <2418184580@qq.com>
2024-02-16 12:02:21 +0800

    feat: add modules

--- src/modules/erc20-approval/items.ts
+++ src/modules/erc20-approval/items.ts
@@ -9,4 +9,79 @@ export default [
     value: 'era',
     contractAddress: '0xeCD9f240ED3895c77dB676004328Dd1d246f33C9',
   },
+  {
+    title: 'TFT',
+    value: 'tft',
+    contractAddress: '0x14ACCcD04393f26BA155E5402aA6fdDBB8e2254a',
+  },
+  {
+    title: 'SEN',
+    value: 'sen',
+    contractAddress: '0xC2613e94ddfF333685D37b6ac9ad0A1b2212f102',
+  },
+  {
+    title: 'BAN',
+    value: 'ban',
+    contractAddress: '0x1DB930b5ef1Ba968Cc11Ed72F6f397B15de5fF4f',
+  },
+  {
+    title: 'CAT',
+    value: 'cat',
+    contractAddress: '0x5C50eE5CAD80593266cD4010b6f6B5635825E48A',
+  },
+  {
+    title: 'GEJU',
+    value: 'geju',
+    contractAddress: '0x98141754F0148A34F59f46744503eDB1A2b1e29D',
+  },
+  {
+    title: 'GRE',
+    value: 'gre',
+    contractAddress: '0x4e26Cda58dAa8111cEA612260453529a42289f0a',
+  },
+  {
+    title: 'LOLITA',
+    value: 'lolita',
+    contractAddress: '0xd3Cabe35e43564ecEa3BF3d1a8b0aC6D16400e03',
+  },
+  {
+    title: 'CHD',
+    value: 'chd',
+    contractAddress: '0x730A32f5e17Bad319703Cc55A331D0b95d1C2a7d',
+  },
+  {
+    title: 'ZKDC',
+    value: 'zkdc',
+    contractAddress: '0xe2c55af390a0f82dd3FE29d6B31Df2f756f7deCD',
+  },
+  {
+    title: 'ZUP',
+    value: 'zup',
+    contractAddress: '0x5766f25E052787D1d7498ebe1aA5C623d8078bca',
+  },
+  {
+    title: 'LIBERTAS',
+    value: 'libertas',
+    contractAddress: '0xD387869f5Ed65e6c6dDFf5caB2724B96fa4E5386',
+  },
+  {
+    title: 'PPT',
+    value: 'ppt',
+    contractAddress: '0x030B8487c5f5b77193b53e56F951865B79358e30',
+  },
+  {
+    title: 'xSPACE',
+    value: 'xspace',
+    contractAddress: '0x6aF43486Cb84bE0e3EDdCef93d3C43Ef0C5F63b1',
+  },
+  {
+    title: 'ZERC1',
+    value: 'zerc1',
+    contractAddress: '0x9AD4c4d0800831ED69Ab1289df25280EF22801ba',
+  },
+  {
+    title: 'ZERC2',
+    value: 'zerc2',
+    contractAddress: '0xa6f65bbBB723155074A1c83f25B07Ec1F866C706',
+  },
 ]
